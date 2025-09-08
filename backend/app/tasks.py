7e8a6ad5f8a23b065f984f042004adb752bea496
MANNAN HOSSAIN <153767329+Mannanhossain@users.noreply.github.com>
2025-09-08 11:56:08 +0530

    update code

--- backend/app/tasks.py
+++ backend/app/tasks.py
@@ -11,14 +11,14 @@ TWILIO_NUMBER = os.getenv("TWILIO_NUMBER")
 client = Client(TWILIO_SID, TWILIO_AUTH_TOKEN)
 
 def handle_missed_call(payload):
-    # Save to DB
+
     db = SessionLocal()
     missed_call = MissedCallModel(caller_number=payload.caller_number, timestamp=payload.timestamp)
     db.add(missed_call)
     db.commit()
     db.close()
 
-    # Send SMS
+   
     if TWILIO_SID and TWILIO_AUTH_TOKEN:
         client.messages.create(
             body=f"You missed a call from {payload.caller_number}",
